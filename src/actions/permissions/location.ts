269aecd2c1366c7ba983b4ac043e61eda68ca1b1
yerytech <yerytecnology@gmail.com>
2025-02-04 12:15:26 -0400

    Add checkLocationPermission function to verify location permission status

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -5,6 +5,7 @@ import {
   PERMISSIONS,
   PermissionStatus as RnPermissionsStatus,
   request,
+  check,
 } from "react-native-permissions";
 import { PermissionStatus } from "../../infrastructure/interfaces/permissions";
 
@@ -32,3 +33,29 @@ export const requestLocationPermission =
     };
     return permissionMapper[status] ?? "unavailable";
   };
+
+export const checkLocationPermission = async (): Promise<PermissionStatus> => {
+  let status: RnPermissionsStatus = "denied";
+
+  if (Platform.OS === "ios") {
+    status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+  } else if (Platform.OS === "android") {
+    status = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+  } else {
+    throw new Error("Platform not supported");
+  }
+  if (status === "blocked") {
+    await openSettings();
+  }
+  const permissionMapper: Record<RnPermissionsStatus, PermissionStatus> = {
+    granted: "granted",
+    denied: "denied",
+    blocked: "blocked",
+    limited: "limited",
+    unavailable: "unavailable",
+  };
+  return permissionMapper[status] ?? "unavailable";
+};
+
+  
+
