a58f7db4bec1468bbf6be7adb9b3c42dd0037b8a
yerytech <yerytecnology@gmail.com>
2025-02-05 15:57:31 -0400

    Integrate Google Maps API and update MapScreen to display a map view

--- ios/MapsApp/AppDelegate.mm
+++ ios/MapsApp/AppDelegate.mm
@@ -2,11 +2,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDFHWiIj2QeD4c6Yf_O9I_8cbAl9EzZLio"];
   self.moduleName = @"main";
 
   // You can add your custom initial props in the dictionary below.
--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,8 +1,31 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 export const MapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      ></MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
